cde1767c5b9f74d6e55e0481d89c18c25aa91783
CaMapCon <kien.nguyentrung1699@gmail.com>
2025-04-02 22:50:44 +0700

    implement auth and storage

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -10,6 +10,9 @@ import Link from "next/link";
 import {toast} from "sonner";
 import FormField from "@/components/FormField";
 import {useRouter} from "next/navigation";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from "@firebase/auth";
+import {signIn, signUp} from "@/lib/actions/auth.action";
+import { auth } from "@/firebase/client";
 
 const authFormSchema = (type:FormType) =>{
 
@@ -34,12 +37,41 @@ const AuthForm = ({type}: {type: FormType}) => {
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    async function onSubmit(values: z.infer<typeof formSchema>) {
         try{
         if(type === 'sign-up'){
+            const {name, email, password} = values;
+
+            const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+
+            const result = await signUp({
+                uid: userCredentials.user.uid,
+                name: name!,
+                email: email,
+                password: password,
+            })
+
+            if(!result?.success){
+                toast.error(result?.message);
+                return;
+            }
+
             toast.success('Account created successfully. Please sign in.');
             router.push('/sign-in');
         }else {
+            const {email, password} = values;
+
+            const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+            const idToken = await userCredentials.user.getIdToken();
+
+            if(!idToken){
+                toast.error('There was an error signing in.');
+                return;
+            }
+            await signIn({
+                email, idToken
+            })
+
             toast.success('Sign in successfully.');
             router.push('/');        }
         }catch (error){
