c91dd65334442e486f0b46b6113a04bcac86e41c
CaMapCon <kien.nguyentrung1699@gmail.com>
2025-04-07 00:09:14 +0700

    implement interview session

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
 import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 
-const Page = () => {
+const Page = async () => {
+    const user = await getCurrentUser();
+
     return (
-       <>
-           <div>Interview Generation</div>
+        <>
+            <h3>Interview generation</h3>
 
-           <Agent userName="Kien" userId="user1" type="generate"/>
-       </>
+            <Agent
+                userName={user?.name!}
+                userId={user?.id}
+                profileImage={user?.profileURL}
+                type="generate"
+            />
+        </>
+    );
+};
 
-    )
-}
-export default Page
+export default Page;
